a808a30dea557b155f6f84c272775ffeeb30eb27
Hanna Nesterova <nesterova.hanna@gmail.com>
2023-01-04 12:54:03 +0100

    Create let for bills and use these variables for argCompiterMove and argPlayerMove

--- js/script.js
+++ js/script.js
@@ -22,29 +22,34 @@ function playGame (playerInput) {
     const playerMove = getMoveName(Number(playerInput));
     const argComputerMove = computerMove;
     const argPlayerMove = playerMove;
+    let myBills = 0;
+    myBills ++;
+    let compBills = 0;
+    compBills ++;
+  
+
+
     function displayResult (argComputerMove , argPlayerMove){
         printMessage('Компютер обрав ' + argComputerMove + ', а ти обрав ' + argPlayerMove);
             if( argComputerMove === 'камінь' && argPlayerMove === 'папір'){
-                printMessage('Вітаю, ти переміг!');
-                
+                printMessage('Вітаю, ти переміг! Твої бали : ' + myBills);
             }
             else if( argComputerMove === 'ножиці' && argPlayerMove === 'камінь'){
-                printMessage('Вітаю, ти переміг!');
+                printMessage('Вітаю, ти переміг!Твої бали : '+ myBills);
             }
             else if( argComputerMove === 'папір' && argPlayerMove === 'ножиці'){
-            printMessage('Вітаю, ти переміг!');
+            printMessage('Вітаю, ти переміг!Твої бали :'+ myBills);
             }
             else if( argComputerMove === 'папір' && argPlayerMove === 'папір' || computerMove === 'ножиці' && playerMove === 'ножиці' || computerMove === 'камінь' && playerMove === 'камінь'){
             printMessage('Нечія,спробуйте ще раз.');
             }
             else{
-            printMessage('Нажаль ти програв,спробуйте ще...');
+            printMessage('Нажаль ти програв,спробуйте ще...Бали компютера : ' + compBills);
             }
         }
         displayResult (argComputerMove , argPlayerMove);
 }
 
-// playGame( );
 
 const playRock = document.querySelector('#play-rock');
 const playPaper = document.querySelector('#play-paper');
@@ -62,28 +67,31 @@ playScissors?.addEventListener('click',function () {
     playGame (3)
 });
 
-const gameRules = {
-    "камінь" : {
-        "камінь" :  0,
-        "ножиці" : 1,  
-        "папір" : 0, //-1,
-     }, 
-    "ножниці" : {
-        "камінь" :  0,//-1,
-        "ножиці" : 0,  
-        "папір" : 1,
-    }, 
-    "папір" : {
-        "камінь" :  1,
-        "ножиці" : 0, //-1,  
-        "папір" : 0,
-    } 
-}
 
-let i =0;
-function countBills (){
-    if (argComputerMove === 'камінь' && argPlayerMove === 'папір'){
-        printBills('Вітаю, ти переміг!');
-        i++;
-    }
-}
+
+//COUNT BILLS
+// const gameRules = {
+//     "камінь" : {
+//         "камінь" :  0,
+//         "ножиці" : 1,  
+//         "папір" : 0, //-1,
+//      }, 
+//     "ножниці" : {
+//         "камінь" :  0,//-1,
+//         "ножиці" : 0,  
+//         "папір" : 1,
+//     }, 
+//     "папір" : {
+//         "камінь" :  1,
+//         "ножиці" : 0, //-1,  
+//         "папір" : 0,
+//     } 
+// }
+
+// let i =0;
+// function countBills (){
+//     if (argComputerMove === 'камінь' && argPlayerMove === 'папір'){
+//         printBills('Вітаю, ти переміг!');
+//         i++;
+//     }
+// }
