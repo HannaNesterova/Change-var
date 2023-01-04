0a29e77d7b9d03588c3f7ec3151d60917b7a0b45
Hanna Nesterova <nesterova.hanna@gmail.com>
2023-01-04 13:08:38 +0100

    Replase let myBills/compBills outside the function

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
 // Computer choises
+
+
+
+let myBills = 0;
+let compBills = 0;
+
+
 function playGame (playerInput) {
     clearMessages();
     const roundNumber = Math.floor(Math.random() * 3 + 1);
@@ -20,31 +27,27 @@ function playGame (playerInput) {
     }
     // Player
     const playerMove = getMoveName(Number(playerInput));
-    const argComputerMove = computerMove;
-    const argPlayerMove = playerMove;
-    let myBills = 0;
-    myBills ++;
-    let compBills = 0;
-    compBills ++;
+
+ 
   
-
-
+    const argPlayerMove = playerMove;
+    const argComputerMove = computerMove;
     function displayResult (argComputerMove , argPlayerMove){
         printMessage('Компютер обрав ' + argComputerMove + ', а ти обрав ' + argPlayerMove);
             if( argComputerMove === 'камінь' && argPlayerMove === 'папір'){
-                printMessage('Вітаю, ти переміг! Твої бали : ' + myBills);
+                printMessage('Вітаю, ти переміг! Твої бали : ' + myBills+1);
             }
             else if( argComputerMove === 'ножиці' && argPlayerMove === 'камінь'){
-                printMessage('Вітаю, ти переміг!Твої бали : '+ myBills);
+                printMessage('Вітаю, ти переміг!Твої бали : '+ myBills+1);
             }
             else if( argComputerMove === 'папір' && argPlayerMove === 'ножиці'){
-            printMessage('Вітаю, ти переміг!Твої бали :'+ myBills);
+            printMessage('Вітаю, ти переміг!Твої бали :'+ myBills+1);
             }
             else if( argComputerMove === 'папір' && argPlayerMove === 'папір' || computerMove === 'ножиці' && playerMove === 'ножиці' || computerMove === 'камінь' && playerMove === 'камінь'){
             printMessage('Нечія,спробуйте ще раз.');
             }
             else{
-            printMessage('Нажаль ти програв,спробуйте ще...Бали компютера : ' + compBills);
+            printMessage('Нажаль ти програв,спробуйте ще...Бали компютера : ' + compBills+1);
             }
         }
         displayResult (argComputerMove , argPlayerMove);
